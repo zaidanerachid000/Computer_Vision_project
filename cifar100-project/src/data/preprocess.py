cdd87fc42f1847e939ed48a68649752995340b00
ayoubaoubid <ayoubaoubid3@gmail.com>
2026-04-08 13:29:50 +0100

    preprocessing

--- cifar100-project/src/data/preprocess.py
+++ cifar100-project/src/data/preprocess.py
@@ -1,1 +1,12 @@
 
+import numpy as np
+
+def normalize(X):
+    return X / 255.0
+
+
+def reshape_images(X):
+    # (N, 3072) → (N, 32, 32, 3)
+    X = X.reshape(-1, 3, 32, 32)
+    X = np.transpose(X, (0, 2, 3, 1))
+    return X
